a0f9aee47d5df4a26958ee49d23b0e9341de3ff5
yanatori <yanatori333@gmail.com>
2022-09-02 21:06:04 +0900

    fix:comment

---
+++ prisma/migrations/20220902115342_v7/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `comment` on the `Coffee` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Coffee" DROP COLUMN "comment";
